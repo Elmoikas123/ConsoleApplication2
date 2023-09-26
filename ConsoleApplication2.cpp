f0c53595f3b2f76e160ea4ca4502e43ae48b7bfd
Alumno <Alumno@PC-B02>
2023-09-26 10:06:32 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+#include <iostream >
+using namespace std;
+const int NUM = 8;
+int main()
+{
+int nums[NUM];
+int total = 0;
+for (int i = 0; i < NUM; i++)
+{
+	cout << "Por favor, introduzca el número";
+	cin >> nums[i]; 
+	total += nums [i];
+}
+cout <<"El total de números es" <<total << endl;
+system("pause");
+return 0;
+}
